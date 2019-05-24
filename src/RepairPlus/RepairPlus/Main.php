558d50a2a44fb4c451db9a4db5d1c672ae815c37
jhampt <49539815+jhampt@users.noreply.github.com>
2019-05-24 11:03:41 -0400

    Update Main.php

--- src/RepairPlus/RepairPlus/Main.php
+++ src/RepairPlus/RepairPlus/Main.php
@@ -1,1 +1,61 @@
+<?php
 
+namespace RepairPlus\RepairPlus;
+
+use pocketmine\Server;
+
+use pocketmine\Player;
+
+use pocketmine\plugin\PluginBase;
+
+use jojoe77777\FormAPI;
+
+use pocketmine\command\Command;
+
+use pocketmine\command\CommandSender;
+
+use pocketmine\event\Listener;
+
+use pocketmine\event\server\ServerCommandEvent;
+
+class EnchantmentInfo extends PluginBase implements Listener{
+    public function onEnable(): void{
+        $this->getServer()->getPluginManager()->registerEvents(($this), $this);
+        $this->getLogger()->info("RepairPlus Enabled By jhampt");
+    }
+  
+    public function onDisable(): void{
+        $this->getLogger()->info("RepairPlus Disabled By jhampt");
+    }
+  
+    public function checkDepends(): void{
+        $this->formapi = $this->getServer()->getPluginManager()->getPlugin("FormAPI");
+        if(is_null($this->formapi)){
+            $this->getLogger()->error("RepairPlus Requires FormAPI To Work Correctly");
+            $this->getPluginLoader()->disablePlugin($this);
+        }
+    }
+  
+    public function onCommand(CommandSender $sender, Command $cmd, string $label, array $args):bool{
+        if($cmd->getName() == "repair"){
+            if(!($sender instanceof Player)){
+                $sender->sendMessage("RepairPlus", false);
+                return true;
+            }
+            $api = $this->getServer()->getPluginManager()->getPlugin("FormAPI");
+            $form = $api->createSimpleForm(function (Player $sender, $data){
+                $result = $data;
+                if ($result == null) {
+                }
+                switch ($result) {
+                }
+            });
+            $form->setTitle("RepairPlus");
+            $form->setContent("Repair Below:\nEnter The Damage Amount You Want To Change Your Item To");
+            $form->addInput("");
+            $form->addButton("Confirm");
+            $form->sendToPlayer($sender);
+        }
+        return true;
+    }
+}
